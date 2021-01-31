89d0b2e2e43aedf15d4dc17b8ec3db1b38d23421
vlad <vladimir.shkodin@slickdeals.net>
2021-01-30 17:55:53 -0800

    init commit

---
+++ readme.md
@@ -0,0 +1,17 @@
+## Simple example of UI Testing Framework using Python, Pytest, and Playwright.
+#### Clone project
+```
+git clone
+cd 
+```
+### install
+```
+pip install -r requirements.txt
+python -m playwright install
+```
+### How to run
+```
+pytest -v
+```
+Please give a "star" if it helped you.
+
---
+++ test_anyName.py
@@ -0,0 +1,11 @@
+from playwright.sync_api import sync_playwright
+
+
+def test_anyName():
+	with sync_playwright() as p:
+    		for browser_type in [p.chromium, p.firefox, p.webkit]:
+        		browser = browser_type.launch()
+        		page = browser.new_page()
+       			page.goto('http://whatsmyuseragent.org/')
+        		page.screenshot(path=f'example-{browser_type.name}.png')
+       			browser.close()
